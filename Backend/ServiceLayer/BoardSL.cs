de5c5ffd9918012a45e500fab1a067f563d7d2d6
Yuval Gutman <gutmay@post.bgu.ac.il>
2026-04-29 21:28:35 +0300

    Add BoardSL class

---
+++ Backend/ServiceLayer/BoardSL.cs
@@ -0,0 +1,19 @@
+﻿namespace IntroSE.Kanban.Backend.ServiceLayer
+{
+    /// <summary>
+    /// Class representing a board in the service layer.
+    /// </summary>
+    public class BoardSL
+    {
+        public string BoardName { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BoardSL"/> class.
+        /// </summary>
+        /// <param name="boardBL"></param>
+        internal BoardSL(BoardBL boardBL)
+        {
+            this.BoardName = boardBL.BoardBL;
+        }
+    }
+}
