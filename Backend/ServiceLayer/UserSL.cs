7c0ccf7ac40b7127a5f9553c9d5e782997acdd2a
Guy Biran <guybir@post.bgu.ac.il>
2026-04-29 21:01:00 +0300

    Add UserSL class

---
+++ Backend/ServiceLayer/UserSL.cs
@@ -0,0 +1,19 @@
+﻿namespace IntroSE.Kanban.Backend.ServiceLayer
+{
+    /// <summary>
+    /// Class representing a user in the service layer.
+    /// </summary>
+    public class UserSL
+    {
+        public string Email { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserSL"/> class.
+        /// </summary>
+        /// <param name="userBL"></param>
+        internal UserSL(UserBL userBL)
+        {
+            this.Email = userBL.Email;
+        }
+    }
+}
