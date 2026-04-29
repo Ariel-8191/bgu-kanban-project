2dcfbd23bdcaee0d21a1a078d0df5d3aa54596da
Guy Biran <guybir@post.bgu.ac.il>
2026-04-29 21:40:57 +0300

    Add Response class

---
+++ Backend/ServiceLayer/Response.cs
@@ -0,0 +1,36 @@
+﻿namespace IntroSE.Kanban.Backend.ServiceLayer
+{
+    /// <summary>
+    /// Class representing a response from the service layer.
+    /// </summary>
+    public class Response<T>
+    {
+        public string ErrorMessage { get; }
+        public T ReturnValue { get; }
+
+        /// <summary>
+        /// Create an empty response.
+        /// </summary>
+        public Response()
+        {
+
+        }
+
+        /// <summary>
+        /// Create an error message response.
+        /// </summary>
+        public Response(string errorMessage)
+        {
+            this.ErrorMessage = errorMessage;
+        }
+
+        /// <summary>
+        /// Create a response with a return value.
+        /// </summary>
+        public Response(T returnValue)
+        {
+            this.ReturnValue = returnValue;
+        }
+
+    }
+}
