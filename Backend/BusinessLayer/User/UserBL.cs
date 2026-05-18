f4138b5af8d18f7ac83fe6b87ef4876560049e8b
Ariel Shapira <shapari@post.bgu.ac.il>
2026-05-18 16:54:23 +0300

    add UserBL skeleton

---
+++ Backend/BusinessLayer/User/UserBL.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace IntroSE.Kanban.Backend.BusinessLayer.User
+{
+    internal class UserBL
+    {
+        public string Email { get; }
+        private string _password;
+
+        private string Password
+        {
+            get { return _password; }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+       
+        public UserBL(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckPassword(string password)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
