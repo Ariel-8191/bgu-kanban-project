069601958f9f45d28dbd4de6fb5dfc37d6b684da
Guy Biran <guybir@post.bgu.ac.il>
2026-05-18 16:54:03 +0300

    Add UserFacade skeleton

---
+++ Backend/BusinessLayer/User/UserFacade.cs
@@ -0,0 +1,32 @@
+﻿using IntroSE.Kanban.Backend.BusinessLayer.CrossCutting;
+using System;
+using System.Collections.Generic;
+
+namespace IntroSE.Kanban.Backend.BusinessLayer.User
+{
+    internal class UserFacade
+    {
+        private Dictionary<string, UserBL> users;
+        private AuthenticationFacade authenticationFacade;
+
+        public UserFacade()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserBL Register(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserBL Login(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Logout(string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
