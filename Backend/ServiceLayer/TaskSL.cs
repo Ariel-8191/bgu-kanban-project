2e8914ac09312196bc87fa7ee6b3de83d97cfc6d
Yuval Gutman <73075158+Yuval1Gutman@users.noreply.github.com>
2026-05-27 22:24:48 +0300

    Revert "Revert accidental merge of board service tests into main"

---
+++ Backend/ServiceLayer/TaskSL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace IntroSE.Kanban.Backend.ServiceLayer
+{
+    /// <summary>
+    /// Represents a task in the service layer.
+    /// </summary>
+    public class TaskSL
+    {
+        public long TaskID { get; }
+        public DateTime CreationTime { get; }
+        public string Title { get; }
+        public DateTime DueDate { get; }
+        public string Description { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TaskSL"/> class.
+        /// </summary>
+        /// <param name="taskBL">The task instance from the businnes layer that the new instance represents</param>
+        internal TaskSL(TaskBL taskBL)
+        {
+            this.TaskID = taskBL.TaskID;
+            this.CreationTime = taskBL.CreationTime;
+            this.Title = taskBL.Title;
+            this.DueDate = taskBL.DueDate;
+            this.Description = taskBL.Description;
+        }
+    }
+}
